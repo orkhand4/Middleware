d068c2b37daa11b5588cbcf51467b380d5cf8080
Orkhand4 <orkhand4@mail.ru>
2025-04-05 12:20:01 +0400

    done

--- app/middleware.js
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-
-export const middleware = (req) => {
-  return NextResponse.redirect(new URL("/", req.url));
-  return NextResponse.next();
-};
---
+++ middleware.js
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request) {
+  const { pathname } = request.nextUrl;
+
+  if (pathname === "/contact") {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  return NextResponse.next();
+}
