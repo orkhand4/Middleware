cf1de21cb5126ada5330511774bd54fd66bc1889
Orkhand4 <orkhand4@mail.ru>
2025-04-06 11:25:56 +0400

    POST method

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,26 +1,17 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
 const products = [
-  { id: 1, name: 'Product 1', price: 100 },
-  { id: 2, name: 'Product 2', price: 200 },
-  { id: 3, name: 'Product 3', price: 300 },
+  { id: 1, name: "Product 1", price: 100 },
+  { id: 2, name: "Product 2", price: 200 },
+  { id: 3, name: "Product 3", price: 300 },
 ];
 
-
-export function GET(req,res){
-
-  return NextResponse.json({data:products})
-
+export function GET(req, res) {
+  return NextResponse.json({ data: products });
 }
 
-export function POST(req,res){
-
-  return NextResponse.json({data:'This is a post request'})
-
-}
-
-export function DELETE(req,res){
-
-  return NextResponse.json({data:'This is a delete request'})
-
+export async function POST(req, res) {
+  const data = await req.json();
+  products.push(data);
+  return NextResponse.json({ data: products }, { status: 201 });
 }
